30b470b6f32ddb60a75263f2e7ba66358f98312a
Antonio Thiago <antoniobackenddeveloper@gmail.com>
2023-01-05 15:56:22 -0300

    setup initial

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv");
+const path = require("path");
+const express = require("express");
+const app = express();
+
+dotenv.config();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.render("pages/index");
+});
+
+app.listen(process.env.PORT, () => console.log("run"));
